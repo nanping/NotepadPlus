bd46204968e7f9225297b8c30e0cc78e2c2fb774
ping nan <nanping.hu@outlook.com>
2020-01-11 10:06:33 +0800

    Signed-off-by: ping nan <nanping.hu@outlook.com>

---
+++ upload_files.bat
@@ -0,0 +1,14 @@
+git add .
+git commit -m ""
+git push github master
+git pull github master
+
+git push gitee master
+git pull gitee master
+
+
+::从github拉取更新
+::git pull github
+
+::从gitee拉取更新
+::git pull gitee
